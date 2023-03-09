dd5c48902ae24ca30f81408a164445477698e70f
Aditya Panditrao <118619950+assistaditya@users.noreply.github.com>
2023-03-09 21:55:54 +0530

    Create  Program to Display Number.cpp

---
+++ Program to Display Number.cpp
@@ -0,0 +1,17 @@
+/*  C++ Program to Display Number (Entered by the User)  */
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int number;
+    
+    cout<<"\n Enter an interger = ";
+    cin>>number;
+    
+    cout<<"\n The number = "<<number<<"\n ";
+    
+    return 0;
+}
