9a86f4eba778f80fa8953d804876b092254f5693
Aditya Panditrao <118619950+assistaditya@users.noreply.github.com>
2023-03-09 22:03:20 +0530

    Create Program to addition of two numbers.cpp

---
+++ Program to addition of two numbers.cpp
@@ -0,0 +1,22 @@
+/*  C++ Program to Addition of Two Numbers  */
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int a,b,sum;
+    
+    cout<<"\n Enter the first number a = ";
+    cin>>a;
+    
+    cout<<"\n Enter the second number b =";
+    cin>>b;
+    
+    sum=a+b;
+    
+    cout<<"\n Sum of a & b = "<<sum;
+    
+    return 0;
+}
